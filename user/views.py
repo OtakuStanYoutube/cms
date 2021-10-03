06fb37c79cbc4bd37c598ad9615fcb5e03e4ffa4
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-03 17:59:34 +0530

    :gear: Restricting views for unauthenticated users

--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.messages import error, success
-from user.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='login-view')
 def home_page(request):
     return render(request, 'user/dashboard.html')
 

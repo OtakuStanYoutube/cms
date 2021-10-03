2a7bda500560976744e339618d2e782199727fc8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-03 19:21:39 +0530

    :gear: restricting authenticated user from going back to login view

--- user/views.py
+++ user/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.messages import error, success
 from django.contrib.auth.decorators import login_required
+from .authDecorators import unauthenticated_user
 
 # Create your views here.
 @login_required(login_url='login-view')
 def home_page(request):
     return render(request, 'user/dashboard.html')
 
+@unauthenticated_user
 def login_page(request):
     if request.method == 'POST':
         email = request.POST.get('email')

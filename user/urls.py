434951df1c5504da814563899b4491cc220145e3
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-03 18:38:08 +0530

    :railway_track: Adding logout user view to url path

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_page, login_page
+from .views import home_page, login_page, logout_page
 
 urlpatterns = [
     path('', home_page, name='home-view'),
-    path('login/', login_page, name='login-view'),
+    path('auth/login/', login_page, name='login-view'),
+    path('auth/logout/', logout_page, name='logout-view'),
 ]

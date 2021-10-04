7f1e819fbe14e82bd85ca6aafb9b39a2355230a0
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-04 21:04:08 +0530

    :railway_track: Updating users listing view in urls

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard_view, login_view, logout_view
+from .views import dashboard_view, login_view, logout_view, users_view
 
 urlpatterns = [
     path('', dashboard_view, name='home-view'),
+    path('users/', users_view, name='users-view'),
     path('auth/login/', login_view, name='login-view'),
     path('auth/logout/', logout_view, name='logout-view'),
 ]

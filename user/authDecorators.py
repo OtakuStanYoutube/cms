961d35f75276d5f6e42715027c6c01e97c9358e1
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-04 21:18:26 +0530

    :triangular_ruler: creating utility to restrict non admin users from visiting certain routes

--- user/authDecorators.py
+++ user/authDecorators.py
@@ -5,7 +5,15 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('home')
+            return redirect('home-view')
         else:
             return view_func(request, *args, **kwargs)
+    return wrapper_func
+
+def is_admin(view_func):
+    def wrapper_func(request, *args, **kwargs):
+        if request.user.is_superuser or request.user.role == 'Admin':
+            return view_func(request, *args, **kwargs)
+        else:
+             return redirect('home-view')
     return wrapper_func

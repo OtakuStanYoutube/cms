9be287ffd4498086bdfb6e737ab0d81b3c24022e
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-07 20:05:54 +0530

    :card_index: Implementing user creation forms

--- user/forms.py
+++ user/forms.py
@@ -1,9 +1,9 @@
 from django.forms import EmailField
 from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth.models import User
+from .models import User
 
 class UserForm(UserCreationForm):
     email = EmailField(required=True)
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'role']

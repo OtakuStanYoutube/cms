4995f2094e0c0cc754cc71502beaffde4e94f0fc
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-07 20:18:34 +0530

    :railway_track: Adding update user view to urls

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dashboard_view, login_view, logout_view, users_view, create_user_view
+from .views import dashboard_view, login_view, logout_view, users_view, create_user_view, update_user_view
 
 urlpatterns = [
     path('', dashboard_view, name='home-view'),
     path('users/', users_view, name='users-view'),
     path('users/create', create_user_view, name='user-create-view'),
+    path('users/update/<str:pk>', update_user_view, name='user-update-view'),
     path('auth/login/', login_view, name='login-view'),
     path('auth/logout/', logout_view, name='logout-view'),
 ]

af50d861f7856c3a6e54c2a9facf49b6f3714da2
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-13 20:32:11 +0530

    :gear: Adding sponsor view

--- sponsors/views.py
+++ sponsors/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Sponsor, Sponsors
 
 # Create your views here.
+def sponsors_view(request):
+    sponsors = Sponsors.objects.all()
+    context = {
+        'sponsors': sponsors
+    }
+    return render(request, 'sponsors/sponsors.html', context=context)

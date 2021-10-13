ed8fc787f823d3a54aeddbe2b54ed9ea4892979d
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-13 20:03:04 +0530

    :bricks: Updating sponsor model

--- sponsors/models.py
+++ sponsors/models.py
@@ -3,15 +3,22 @@ from helpers.models import BaseModel
 
 # Create your models here.
 class Sponsors(BaseModel):
-    CATEGORY = (
+    SPONSOR_CATEGORY = (
         ('Sponsored', 'Sponsored'),
         ('Integrated', 'Integrated'),
         ('Dedicated', 'Dedicated'),
     )
-    name = models.CharField(max_length=100)
+    
+    SPONSORED_DOMAIN = (
+        ('Youtube', 'Youtube'),
+        ('Podcast', 'Podcast'),
+        ('Website', 'Website'),
+    )
+    name = models.CharField(max_length=100, db_index=True)
     description = models.TextField()
     domain = models.URLField(blank=True)
-    sponsor_type = models.CharField(max_length=100, choices=CATEGORY)
+    sponsor_type = models.CharField(max_length=100, choices=SPONSOR_CATEGORY, db_index=True)
+    sponsored_domain = models.CharField(max_length=100, choices=SPONSORED_DOMAIN, db_index=True)
     amount = models.IntegerField(default=0)
 
     def __str__(self):

a7ed20e6ea40a1230f820c814da4ef7c11d0f7c8
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-13 19:54:20 +0530

    :bricks: Creating sponsor model

--- sponsors/models.py
+++ sponsors/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from helpers.models import BaseModel
 
 # Create your models here.
+class Sponsors(BaseModel):
+    CATEGORY = (
+        ('Sponsored', 'Sponsored'),
+        ('Integrated', 'Integrated'),
+        ('Dedicated', 'Dedicated'),
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    domain = models.URLField(blank=True)
+    sponsor_type = models.CharField(max_length=100, choices=CATEGORY)
+    amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name

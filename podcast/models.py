b2f48d9a38017aab1c984bd5cfcaf97661bc5da3
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-13 19:33:00 +0530

    :bricks: Creating podcast model

--- podcast/models.py
+++ podcast/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# class Podcast(models.Model):
+#     title = models.CharField(max_length=200)
+#     description = models.TextField()
+#     embed_link = models.URLField()
+#     episode_count = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)

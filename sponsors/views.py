b60f0104228d9ae23cc06322bfd8a3e476e3c07b
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-17 20:31:46 +0530

    :gear: Adding create sponsors view for new sponsors

--- sponsors/views.py
+++ sponsors/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Sponsors
-
+from .forms import SponsorForm
+from django.contrib.messages import error, success
 # Create your views here.
 def sponsors_view(request):
     sponsors = Sponsors.objects.all()
@@ -8,3 +9,17 @@ def sponsors_view(request):
         'sponsors': sponsors
     }
     return render(request, 'sponsors/sponsors.html', context=context)
+
+
+def create_sponsor_view(request):
+    sponsor_form = SponsorForm()
+    if request.method == 'POST':
+        sponsor_form = SponsorForm(request.POST)
+        if sponsor_form.is_valid():
+            sponsor_form.save()
+            success(request, 'Sponsor Created successfully.')
+            redirect('sponsors-view')
+    context = {
+        'form': sponsor_form
+    }
+    return render(request, 'sponsors/create_sponsor.html', context)

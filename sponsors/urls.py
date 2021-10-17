a9d75326da718a1b9f2acebcdf0c0bc08c518478
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-17 20:46:27 +0530

    :railway_track: Updating create sponsors view in urls

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -3,5 +3,5 @@ from .views import sponsors_view, create_sponsor_view
 
 urlpatterns = [
     path('', sponsors_view, name='sponsors-view'),
-    path('/create-sponsors', create_sponsor_view, name='create-sponsors-view'),
+    path('create-sponsors/', create_sponsor_view, name='create-sponsors-view'),
 ]

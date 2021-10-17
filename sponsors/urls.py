79f2373ad04f5d66589672a6b1f3ab3ed90e5f32
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-17 20:38:03 +0530

    :railway_track: Adding create sponsors view to urls

--- sponsors/urls.py
+++ sponsors/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import sponsors_view
+from .views import sponsors_view, create_sponsor_view
 
 urlpatterns = [
     path('', sponsors_view, name='sponsors-view'),
+    path('/create-sponsors', create_sponsor_view, name='create-sponsors-view'),
 ]
